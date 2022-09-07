54dde22137ef4ddf046ea9556a149372d31e3958
PostFace <ijj5258@naver.com>
2022-09-07 15:19:54 +0900

    돌아가게는 수정했습니다. 물의를 일으켜 죄송할 따름...

--- src/main/java/lcategory/controller/LcategoryInsertController.java
+++ src/main/java/lcategory/controller/LcategoryInsertController.java
@@ -1,5 +1,22 @@
 package lcategory.controller;
 
-public class LcategoryInsertController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lcategory.model.LcategoryDao;
+
+@Controller
+public class LcategoryInsertController {
+	private final String command="insert.lcate";
+	private String getPage="insertLcate";
+	
+	@Autowired
+	LcategoryDao lcategoryDao;
+	
+	@RequestMapping(value = command)
+	public String getInsert() {
+		
+		return getPage;
+	}
 }
--- src/main/java/lcategory/model/LcategoryDao.java
+++ src/main/java/lcategory/model/LcategoryDao.java
@@ -1,5 +1,8 @@
 package lcategory.model;
 
+import org.springframework.stereotype.Component;
+
+@Component("lcategoryDao")
 public class LcategoryDao {
 
 }
