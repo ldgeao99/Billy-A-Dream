102bff41772bb24974aaa3c91569c023ba6f7296
DG <DG@DESKTOP-1T5GCCU>
2022-09-14 20:02:34 +0900

    ProductBean, ProductDao 추가

---
+++ src/main/java/product/model/ProductDao.java
@@ -0,0 +1,18 @@
+package product.model;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductDao {
+private final String namespace = "product.model.SQL"; // product.xml 에서 namespace에 적어준것
+	
+	@Autowired //  root-context.xml에서 만들게 한 객체 주입
+	SqlSession sqlSessionTemplate;
+	
+	public int insertProduct(ProductBean pbean){
+		int cnt = sqlSessionTemplate.insert(namespace + ".InsertProduct", pbean);
+		return cnt;
+	}
+}
