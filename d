57d15a33d08a3b39eef3f07b6b22a932a1613952
DG <DG@DESKTOP-1T5GCCU>
2022-09-15 09:41:35 +0900

    Gonie 1차 프로젝트 취합

---
+++ d
@@ -0,0 +1,65 @@
+[33mcommit ab08eb03e9ffecfe043e8cc4314d15156eb51e99[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m, [m[1;31morigin/test[m[33m)[m
+Author: DG <DG@DESKTOP-1T5GCCU>
+Date:   Tue Sep 13 12:21:16 2022 +0900
+
+    pull이 안되는 오류때문에 해야하는 commit
+
+[33mcommit 82029de5d528e9eb08b6e4c99393e68e221f3a32[m
+Author: ChanhoPark-s <chanho159@naver.com>
+Date:   Wed Sep 7 12:36:55 2022 +0900
+
+    chanhotest
+
+[33mcommit 9f3fa7b4106f82c224b50c0c19458599fd83dd5c[m
+Merge: 64e990a 21f3e8d
+Author: PostFace <102017352+PostFace@users.noreply.github.com>
+Date:   Wed Sep 7 12:31:34 2022 +0900
+
+    Merge pull request #2 from PostFace/test
+    
+    test4
+
+[33mcommit 21f3e8d764cac5bd1b55e9288173f04a4dc79daa[m
+Author: DG <DG@DESKTOP-1T5GCCU>
+Date:   Wed Sep 7 12:29:04 2022 +0900
+
+    test4
+
+[33mcommit 64e990a13e17da45f3911acdc9543cffeb21288d[m
+Merge: 8b1867c 163b311
+Author: PostFace <102017352+PostFace@users.noreply.github.com>
+Date:   Wed Sep 7 12:27:37 2022 +0900
+
+    Merge pull request #1 from PostFace/test
+    
+    test3
+
+[33mcommit 163b31119a0114282e16378d279a48bb152c7a5e[m
+Author: DG <DG@DESKTOP-1T5GCCU>
+Date:   Wed Sep 7 12:26:45 2022 +0900
+
+    test3
+
+[33mcommit 8b1867cff1b18b904ca6287fefaa2114b146b58e[m
+Author: DG <DG@DESKTOP-1T5GCCU>
+Date:   Wed Sep 7 12:24:53 2022 +0900
+
+    시험 커밋 2
+
+[33mcommit b6c1ccdf14604d7abdf6bdea6c8eb3cf757c9638[m
+Author: DG <DG@DESKTOP-1T5GCCU>
+Date:   Wed Sep 7 12:16:44 2022 +0900
+
+    주석달고 test 커밋
+
+[33mcommit c4410b9639f615b40602ce88fbc9c4751fcd1027[m
+Author: PostFace <ijj5258@naver.com>
+Date:   Wed Sep 7 11:46:48 2022 +0900
+
+    SqlMapConfiguration 설정
+
+[33mcommit b6b25551d19196f467440725c300483c89a0e43c[m
+Author: PostFace <ijj5258@naver.com>
+Date:   Wed Sep 7 11:28:14 2022 +0900
+
+    첫 설정 web,pom.xml
---
+++ src/main/java/product/controller/ProductInsertController.java
@@ -0,0 +1,93 @@
+package product.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import product.model.ProductBean;
+import product.model.ProductDao;
+
+@Controller
+public class ProductInsertController {
+	
+	private final String command = "insert.prd";
+	private String getPage ="productInsertForm";
+	//private String gotoPage ="redirect:/";
+	
+	@Autowired
+	ProductDao productDao;
+	
+	@Autowired
+	ServletContext servletContext; // 프로젝트 1개당 하나가 자동으로 만들어줌. 그래서 그냥 Autowired만 해줘도 주입됨.
+	
+	@RequestMapping(value = command, method = RequestMethod.GET)
+	public String showInsertForm() {
+		System.out.println("ProductInsertController에 GET 요청 들어옴");
+		return getPage;
+	}
+	
+	@RequestMapping(value = command, method = RequestMethod.POST)
+	public String doInsert(ProductBean pbean) { // 커맨드 객체 형태로 받음
+		System.out.println("ProductInsertController에 POST 요청 들어옴");
+		
+		MultipartFile[] upload = pbean.getUpload();
+		
+		if(upload == null) {
+			System.out.println("넘어온 데이터가 없습니다.");
+		}
+		else {
+			for(MultipartFile multi :upload) {
+				System.out.println(multi.getOriginalFilename());
+				
+				//1. 파일 업로드
+				//System.out.println("multi.getName():" + multi.getName()); // upload : <input> 태그의 name 속성
+				System.out.println("multi.getOriginalFilename():" + multi.getOriginalFilename()); // 내가 선택한 화일의 파일명
+				//System.out.println("pbean.getImage():" + pbean.getImage()); // 이것 또한 내가 선택한 화일명, 이걸 이용해도 상관없음
+				
+				
+				String uploadPath = servletContext.getRealPath("/resources");
+				System.out.println("uploadPath: " + uploadPath);
+				
+				//resources 라는 폴더가 존재하지 않는다명 생성.
+				File folder = new File(uploadPath);
+				if (!folder.exists()) {
+					folder.mkdir(); //폴더 생성합니다.
+					System.out.println(uploadPath + " 경로의 resources 폴더가 생성되었습니다.");
+				}
+				
+				File file = new File(uploadPath + "/" + multi.getOriginalFilename()); // multi.getOriginalFilename() 대신 pbean.getImage() 를 사용해도 됨.
+				try {
+					multi.transferTo(file); // 원하는 위치에 파일을 올리고 싶을 때 사용함. 이 문장 실행과 동시에 업로드 됨.
+					
+				} catch (IllegalStateException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} 
+
+			}
+		}
+		
+		System.out.println(pbean);
+		
+		int cnt = -1;
+		cnt = productDao.insertProduct(pbean);
+		
+		if(cnt > 0 ) {
+			System.out.println("상품 삽입성공");
+		}else {
+			System.out.println("상품 삽입실패");
+		}
+		
+		return getPage;
+	}
+}
