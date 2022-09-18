145ab7d9ee0377d06fe67c38dba4cb9d7beb5506
DG <ldgeao99@gmail.com>
2022-09-19 00:33:34 +0900

    HomeController.java 메인화면에서 상품들 보여지는 기능 추가

--- src/main/java/com/spring/ex/HomeController.java
+++ src/main/java/com/spring/ex/HomeController.java
@@ -1,11 +1,18 @@
 package com.spring.ex;
 
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import product.model.ProductBean;
+import product.model.ProductDao;
 
 /**
  * Handles requests for the application home page.
@@ -15,18 +22,21 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+	private ProductDao productDao;
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
+	public String home(Model model) {
+		
+		List<ProductBean> lists1 = productDao.getRecentProductList();	
+		model.addAttribute("recentProductList", lists1);
+		
+		List<ProductBean> lists2 = productDao.getPopularProductList();	
+		model.addAttribute("popularProductList", lists2);
 		
 		return "home";
 	}
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public String homeRequest() {
-		
-		return "home";
-	}
-	
 }
