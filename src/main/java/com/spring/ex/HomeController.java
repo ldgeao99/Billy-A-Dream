c1eda5771e800ec87898c798412951ec5e87ac1e
DG <ldgeao99@gmail.com>
2022-09-19 00:54:56 +0900

    마지막 Merge

--- src/main/java/com/spring/ex/HomeController.java
+++ src/main/java/com/spring/ex/HomeController.java
@@ -1,6 +1,9 @@
 package com.spring.ex;
 
+
+
 import java.util.List;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,17 +17,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+
 import product.model.ProductBean;
 import product.model.ProductDao;
+
 import lcategory.model.LcategoryBean;
 import lcategory.model.LcategoryDao;
 import scategory.model.ScategoryBean;
 import scategory.model.ScategoryDao;
 
 
-/**
- * Handles requests for the application home page.
- */
+
 @Controller
 public class HomeController {
 	
@@ -38,13 +41,9 @@ public class HomeController {
 	
 	@Autowired
 	ScategoryDao scategoryDao;
-
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Model model) {
-
 		
 		List<ProductBean> lists1 = productDao.getRecentProductList();	
 		model.addAttribute("recentProductList", lists1);
