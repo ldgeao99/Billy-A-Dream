ced19307283199f75454f6012a55e5a14ea3e318
DAEGON LEE <ldgeao99@gmail.com>
2022-11-28 19:09:51 +0900

    Update checkController.java

--- src/main/java/member/controller/checkController.java
+++ src/main/java/member/controller/checkController.java
@@ -26,7 +26,6 @@ public class checkController {
 	
 	
 	
-	//===========// 占쏙옙占싱듸옙 占쌩븝옙체크================================
 	@ResponseBody 
 	@RequestMapping("idcheck.mb")
 	public String idcheck(@RequestParam("id") String id) {
@@ -40,7 +39,6 @@ public class checkController {
 	}
 	
 	
-	//=======// 占쏙옙占싱듸옙 찾占쏙옙===================================
 	@ResponseBody 
 	@RequestMapping("findid.mb")
 	public String FindID(@RequestParam("name") String name,@RequestParam("hp") String hp) {
@@ -59,7 +57,6 @@ public class checkController {
 	}
 	
 	
-	//==========// 占쏙옙橘占싫� 찾占쏙옙==============================================
 	@ResponseBody 
 	@RequestMapping("findpw.mb")
 	public String FindPW(@RequestParam("id") String id,@RequestParam("hp") String hp) throws Exception {
@@ -71,28 +68,27 @@ public class checkController {
 		map.put("hp", hp);
 		
 		int result = mdao.FindPW(map);
-		String temporaryPw = getRandomPassword.tempPassword(8);// 占쏙옙占쏙옙占싸울옙占쏙옙 占쏙옙橘占싫�
+		String temporaryPw = getRandomPassword.tempPassword(8);
 		System.out.println("�엫�떆鍮꾨�踰덊샇 :"+temporaryPw);
-		String securePassword = encoder.encode(temporaryPw);// 占쏙옙占쏙옙占싸울옙占쌔쇽옙 占쏙옙호화 占쏙옙占쏙옙
+		String securePassword = encoder.encode(temporaryPw);
 		
 		if(result>0) {
 			
 			map.put("id", id);
 			map.put("securePassword", securePassword);
 			
-			MemberBean mb = mdao.getById(id);// 占쏙옙占싱듸옙 占쌔댐옙占싹댐옙 占싱몌옙占쏙옙 占쏙옙占쏙옙占싶쇽옙 占쏙옙占쏙옙 占쏙옙占쏙옙占쏙옙
+			MemberBean mb = mdao.getById(id);
 			
 			int cnt = mdao.resetPW(map);
-			System.out.println("占쏙옙橘占싫� 占십깍옙화 占쏙옙占� :"+cnt);
+			System.out.println(""+cnt);
 			
-			sendEMAIL.sendMail(mb.getEmail(), "[Billy A Dream] 占쌈시븝옙橘占싫� 占쌩쇽옙", "[Billy A Dream] 占쌈쏙옙 占쏙옙橘占싫ｏ옙占� ' "+temporaryPw+" ' 占쌉니댐옙.");
+			sendEMAIL.sendMail(mb.getEmail(), "[Billy A Dream]", "[Billy A Dream]  ' "+temporaryPw+" ' .");
 			return "yes";
 		}
 		else {
 			return "null";
 		} 
 	}
-	//==========// 회占쏙옙탈占쏙옙 占쏙옙 占쏙옙橘占싫� 확占쏙옙==============================================
 	@ResponseBody 
 	@RequestMapping("quitPw.mb")
 	public String quitPw(@RequestParam("id") String id,@RequestParam("pw") String pw,HttpSession session) throws Exception {
@@ -102,13 +98,12 @@ public class checkController {
 		MemberBean mb = mdao.getById(id);
 		
 		if(encoder.matches(pw, mb.getPw())){
-			mdao.deleteMember(id);// 회占쏙옙탈占쏙옙
-			session.invalidate(); // 占쏙옙占실된곤옙 占쏙옙 占쏙옙占쏙옙
+			mdao.deleteMember(id);
+			session.invalidate(); 
 			return "yes";
 		}
 		return "null";
 	}
-	//==========// 회占쏙옙탈占쏙옙 占쏙옙 占쏙옙橘占싫� 확占쏙옙==============================================
 	@ResponseBody 
 	@RequestMapping("ipHpCheck.mb")
 	public String ipHpCheck(@RequestParam("id") String id,@RequestParam("hp") String hp,HttpSession session) throws Exception {
