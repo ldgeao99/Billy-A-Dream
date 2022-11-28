5cf7d1ea14393f428c5c7c95da21916587a8c1b1
DAEGON LEE <ldgeao99@gmail.com>
2022-11-28 19:12:08 +0900

    Update memberRegisterController.java

--- src/main/java/member/controller/memberRegisterController.java
+++ src/main/java/member/controller/memberRegisterController.java
@@ -40,29 +40,27 @@ public class memberRegisterController {
 	public String register(MemberBean mb) throws Exception {
 		
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		String securePassword = encoder.encode(mb.getPw());// 占쏙옙占쏙옙占쏙옙 pw 占쏙옙호화
-		Map<String,String>map = new HashMap<String,String>();// ip 占쏙옙臼占� 占십울옙占쏙옙 mno, ip
+		String securePassword = encoder.encode(mb.getPw());
+		Map<String,String>map = new HashMap<String,String>();
 		
 		
 		
-		// 회占쏙옙占쏙옙占쏙옙占쌜억옙
 		mb.setPw(securePassword);
 		mdao.InsertMember(mb); 
 		
 		
-		//ip 占쏙옙占쏙옙訪占�
-		mb = mdao.getById(mb.getId());// mno 占쏙옙 primary key 占쏙옙占쏙옙漫占� mno占쏙옙 占쏙옙占쏙옙占쏙옙占쏙옙 占쏙옙
-		String ip = Inet4Address.getLocalHost().getHostAddress();// 占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙占쏙옙占쏙옙
+		mb = mdao.getById(mb.getId());
+		String ip = Inet4Address.getLocalHost().getHostAddress();
 		
 		map.put("ip", ip);
 		map.put("mno", String.valueOf(mb.getMno()));
 		
 		
-		midao.InsertIp(map); // 占쏙옙占쏙옙占쏙옙 占쏙옙占�
+		midao.InsertIp(map); 
 		
-		String msg = "[Biily A Dream]   "+mb.getName()+"占쏙옙占쏙옙 회占쏙옙占쏙옙占쏙옙占쏙옙 占쏙옙占싹드립占싹댐옙. 占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙占� 占쌉뀐옙 Billy A Dream占쏙옙 占싱뀐옙占쏘갈 占쏙옙占싸곤옙占쏙옙 占실셨쏙옙占싹댐옙. 占쏙옙占쏙옙占쌌니댐옙.";
+		String msg = "[Biily A Dream]   "+mb.getName()+"";
 		
-		//sendEMAIL.sendMail(mb.getEmail(), "[Billy A Dream] 회占쏙옙占쏙옙占쏙옙 占쏙옙占싹드립占싹댐옙.", msg);
+		
 		
 		return gotoPage;
 	}
