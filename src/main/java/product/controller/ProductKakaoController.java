f431bd2759ac4b1be631ca2b240f8fc22ac7115a
DAEGON LEE <ldgeao99@gmail.com>
2022-11-28 18:59:10 +0900

    Update ProductKakaoController.java

--- src/main/java/product/controller/ProductKakaoController.java
+++ src/main/java/product/controller/ProductKakaoController.java
@@ -41,10 +41,9 @@ public class ProductKakaoController {
 							@RequestParam("start_date")	String start_date,	@RequestParam("end_date")	String end_date,@RequestParam("id") String id,
 							HttpSession session) {
 			
-			// 한묶음 묶어서 url 보내기
-			MemberBean mb=  mdao.getById(id); // 아이디에 해당하는 레코드 가져오기
+			MemberBean mb=  mdao.getById(id); 
 			
-			ReservationBean rb = new ReservationBean();  // 이렇게해서 session 처리
+			ReservationBean rb = new ReservationBean();  
 			
 			rb.setProduct_no(no);
 			rb.setBuyer_no(mb.getMno());
@@ -56,33 +55,33 @@ public class ProductKakaoController {
 			
 			try {
 			
-			URL url = new URL("https://kapi.kakao.com/v1/payment/ready"); // 갈 서버
-			HttpURLConnection conn = (HttpURLConnection) url.openConnection();//요청하는 클라이언트랑 요청을 받는 카카오페이서버를 연결해주는 연결다리;
+			URL url = new URL("https://kapi.kakao.com/v1/payment/ready"); 
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("POST");
 			conn.setRequestProperty("Authorization", "KakaoAK 4c0bb93a3847e475f914d28db4a3aabf");
 			conn.setRequestProperty("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
-			conn.setDoOutput(true); // connection에는 doin dooutput 있는데 연결을 통해 서버한테 전해줄 것이 있는가 없는가 있으면 true
+			conn.setDoOutput(true); 
 			String parameter = "cid=TC0ONETIME&partner_order_id=partner_order_id&partner_user_id=partner_user_id&item_name="+pname+"&quantity=1&total_amount="+price+"&vat_amount=200&tax_free_amount=0&approval_url=http://localhost:8080/ex/sucessBuy.prd&fail_url=http://localhost:8080/ex/buy.prd?no="+no+"&cancel_url=http://localhost:8080/ex/buy.prd?no="+no;
-			OutputStream out = conn.getOutputStream(); //서버에 전달해야함 뭔가를 줄 수 있음.
-			DataOutputStream  dataout = new DataOutputStream(out);// 데이터 주는애
-			dataout.writeBytes(parameter); // 문자를 넣어주면 bytes로 바꿔서 해줌. // 가지고있다.
-			dataout.close();//  flush 하고 닫고  가지고있던걸 보낸다.
+			OutputStream out = conn.getOutputStream(); 
+			DataOutputStream  dataout = new DataOutputStream(out);
+			dataout.writeBytes(parameter);
+			dataout.close();
 			
-			int result = conn.getResponseCode();// 통신하는 부분
+			int result = conn.getResponseCode();
 
-			InputStream  in;// 받은애
-			if(result == 200) { // http코드에서 정상적인 코드는 200 , 그외는 에러임
-				in = conn.getInputStream();//받기만함
+			InputStream  in;
+			if(result == 200) { 
+				in = conn.getInputStream();
 			}
 			else {
-				in = conn.getErrorStream(); // 에러를 받는다.
+				in = conn.getErrorStream(); 
 			}
 			
-			InputStreamReader reader = new InputStreamReader(in);// 읽는애
+			InputStreamReader reader = new InputStreamReader(in);
 			
 			
-			BufferedReader br = new BufferedReader(reader);// 형변환함
-			return br.readLine(); // 문자열로 형변환해서 옴
+			BufferedReader br = new BufferedReader(reader);
+			return br.readLine(); 
 			
 			
 		} catch (MalformedURLException e) {
