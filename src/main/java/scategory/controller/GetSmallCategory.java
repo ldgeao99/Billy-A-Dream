5d02d1fab0fc8b59064a6cf8fb8b34cfcacacb01
DAEGON LEE <ldgeao99@gmail.com>
2022-11-28 18:45:44 +0900

    한글주석 제거

--- src/main/java/scategory/controller/GetSmallCategory.java
+++ src/main/java/scategory/controller/GetSmallCategory.java
@@ -22,7 +22,7 @@ public class GetSmallCategory {
 	@Autowired
 	ScategoryDao scategoryDao;
 	
-	@ResponseBody // 해당 메서드가 반환한 것을 jsp 파일이 아닌 객체 그대로 전달한다. 
+	@ResponseBody
 	@RequestMapping(value=command, produces="text/plain;charset=UTF-8")
 	public String getMatchSmallCategory(@RequestParam("lcategoryNo") int lcategoryNo) {
 		
